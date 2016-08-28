8ad2b79a57ad1e704841d7ace2c4c2bde17bd3d1
Yashar Ahmedizade <yashade2001@gmail.com>
2016-08-28 10:35:28 +0000

    enable text relocations

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -32,3 +32,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, device/lge/hammerhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/hammerhead/device-vendor.mk)
+
+TARGET_NEEDS_TEXT_RELOCATIONS := true
